9394229ac956ad0daa1cc0c6123eb84bbf00c7e3
Joseph Meli <joemeli631@gmail.com>
2018-10-04 21:51:32 -0400

    end of day

--- server.js
+++ server.js
@@ -8,63 +8,67 @@ const app = express();
 
 var date = new Date();
 var day = date.getDate();
-var month = date.getMonth() + 1 ;
+var month = date.getMonth() + 1;
 var year = date.getFullYear();
 
-var WOD  = {day: "", workout :""};
+var WOD = { day: '', workout: '' };
 
-app.set('views','./views');
+app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
+  url =
+    'https://www.crossfit.com/workout/' +
+    year +
+    '/' +
+    month +
+    '/' +
+    day +
+    '#/comments';
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+      var workout, day;
 
-	url = 'https://www.crossfit.com/workout/'+ year + '/' + month + '/' + day + '#/comments';
-    request(url, function(error,response, html){
+      workout = $('.content').text();
+      day = $('h1').text();
+      WOD.workout = workout;
+      WOD.day = day;
+      console.log(WOD);
+    } else {
+      console.log(error);
+    }
+  });
 
-		if(!error){
-
-			var $ = cheerio.load(html);
-			var workout , day;
-
-			workout = $('.content').text();
-			day = $('h1').text();
-			WOD.workout = workout;
-			WOD.day = day;
-		    console.log(WOD);
-        }
-		 else{
-			console.log( error);
-		}
-	});
-
-    res.render('index',{heading:WOD.day   , content: 'testomg'  })
+  res.render('index', { heading: WOD.day, content: 'testomg' });
 });
 
-app.get('/scrape', function(req,res){
+app.get('/scrape', function(req, res) {
+  url =
+    'https://www.crossfit.com/workout/' +
+    year +
+    '/' +
+    month +
+    '/' +
+    day +
+    '#/comments';
 
-	url = 'https://www.crossfit.com/workout/'+ year + '/' + month + '/' + day + '#/comments';
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
 
-	request(url, function(error, response, html){
-		if(!error){
+      var workout, day;
+      workout = $('.content').text();
+      day = $('h1').text();
+      WOD.workout = workout;
+      WOD.day = day;
+    } else {
+      console.log(error);
+    }
+  });
 
-			var $ = cheerio.load(html);
-
-			var workout , day;
-			workout = $('.content').text();
-			day = $('h1').text();
-			WOD.workout = workout;
-			WOD.day = day;
-		}
-		 else{
-			console.log( error);
-		}
-	})
-
-	res.send(WOD);
-
-	});
-
-
+  res.send(WOD);
+});
 
 app.listen('8081');
 
